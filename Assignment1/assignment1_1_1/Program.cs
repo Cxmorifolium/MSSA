f8c5dddb07610f75c0cebf7b88c9455d823d8dce
TheNoliOliandPea <161099034+TheNoliOliandPea@users.noreply.github.com>
2025-04-07 10:30:03 -0500

    Initial commit

---
+++ Assignment1/assignment1_1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Create a console application in C# to store and print personal details of a person like name, age and address on console screen. Make use of appropriate variables.
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        // Declare and initialize variables
+        string name = "Cathy Nguyen";
+        int age = 29;
+        string address = "1234 A Street Dr, City, State";
+
+        // Print personal details
+        Console.WriteLine($"Personal Details:\nName: {name}\nAge: {age}\nAddress: {address}");
+    }
+}
