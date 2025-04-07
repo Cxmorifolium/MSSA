f9d8eedf2e670636f66390dee08610acd729c7f7
TheNoliOliandPea <161099034+TheNoliOliandPea@users.noreply.github.com>
2025-04-07 12:08:24 -0500

    added assignments

---
+++ Assignment1/assignment1_2_1/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Write a C# Sharp program to accept two integers and check whether they are equal or not.
+Test Data :
+Input 1st number: 5
+Input 2nd number: 5
+Expected Output :
+5 and 5 are equal
+Test Data :
+Input 1st number: 5
+Input 2nd number: 15
+Expected Output :
+5 and 15 are not equal 
+
+add while loop until user initiates exit for challenge cus I suck at it. Decided to go bool method
+*/
+
+using System;
+class Program {
+    static void Main(string[] args) {
+
+        bool programActive = true;
+
+        //initiate while loop
+        while (programActive) {
+
+            //Ask user to input number. Use ReadLine
+            Console.Write($"Are your numbers equal to each other? Let's find out! \n Please enter your first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Please enter your second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNumber == secondNumber) {
+                Console.Write($"{firstNumber} and {secondNumber} are equal! :) ");
+            }
+
+            else {
+                Console.Write($"{firstNumber} and {secondNumber} are not equal :( ");
+            }
+
+            // handle null/user input
+            while (true) {
+                Console.Write("\nWould you like to compare another? (yes/no): ");
+                string response = Console.ReadLine()?.ToLower() ?? "";
+
+                if (response == "yes") {
+                    break; // exit inner loop and continue
+                } else if (response == "no") {
+                    programActive = false;
+                    Console.WriteLine("Exiting program. Goodbye!");
+                    break;
+                } else {
+                    Console.WriteLine("Please respond with 'yes' or 'no'.");
+                }
+            }
+        }
+    }
+}
---
+++ Assignment1/assignment1_2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Write a C# Sharp program to find the sum of first 10 natural numbers.
+Expected Output :
+The first 10 natural number is :
+1 2 3 4 5 6 7 8 9 10
+The Sum is : 55
+*/
+
+// Method: for loop set from 1-10; initialize sum at 0 
+
+using System;
+
+class Program {
+    static void Main(String[] args) {
+
+        int sum = 0;
+
+        Console.WriteLine("The first 10 natural numbers are: ");
+
+        for (int i = 1; i <=10; i++) {
+            // Write console to show natural number
+            Console.Write(i + " ");
+
+            // Add each number
+            sum += i;
+
+        }
+
+        Console.WriteLine($"\nThe Sum is: {sum}");
+    }
+}
