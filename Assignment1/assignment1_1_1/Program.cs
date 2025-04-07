f430745f6b7155aa02485ae78e075d50a8ded64e
TheNoliOliandPea <161099034+TheNoliOliandPea@users.noreply.github.com>
2025-04-07 15:16:24 -0500

    updated variable names

--- Assignment1/assignment1_1_1/Program.cs
+++ Assignment1/assignment1_1_1/Program.cs
@@ -5,10 +5,10 @@
 class Program {
     static void Main(string[] args) {
         // Declare and initialize variables
-        string name = "Hello Kitty";
+        string userName = "Hello Kitty";
         Console
-        int age = 29;
-        string address = "1234 A Street Dr, City, State";
+        int userAge = 29;
+        string userAddress = "1234 A Street Dr, City, State";
 
         // Print personal details
         Console.WriteLine($"Personal Details:\nName: {name}\nAge: {age}\nAddress: {address}");
@@ -17,20 +17,20 @@ class Program {
 
 */
 
-//user input
+//User input method
 
 using System;
 
 class Program {
     static void Main(string[] args) {
         Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
+        string userName = Console.ReadLine();
 
         Console.Write("Enter your age: ");
-        int age = Convert.ToInt32(Console.ReadLine())!;
+        int userAge = Convert.ToInt32(Console.ReadLine())!;
 
         Console.Write("Enter your address: ");
-        string address = Console.ReadLine();
+        string userAddress = Console.ReadLine();
 
         Console.WriteLine($"\n~*** Personal Details ***~:\n" +
                           $"Name: {name}\n" +
