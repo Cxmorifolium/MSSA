965495556b56e9b76b92a0fe0ab00cb088da6d03
TheNoliOliandPea <161099034+TheNoliOliandPea@users.noreply.github.com>
2025-05-22 14:49:15 -0500

    updates
    updated notes, added assignment

---
+++ Assignment7/assignment7_4/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Leet Code 1603
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //tested on leetcode
+    }
+}
+public class ParkingSystem
+{
+    // Add properties
+    int big;
+    int medium;
+    int small;
+
+    //Constructor
+    public ParkingSystem(int big, int medium, int small)
+    {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    // if-elseif or switchcase
+    public bool AddCar(int carType)
+    {
+        if (carType == 1 && big > 0)
+        {
+            big--;
+            return true;
+        }
+        else if (carType == 2 && medium > 0)
+        {
+            medium--;
+            return true;
+        }
+        else if (carType == 3 && small > 0)
+        {
+            small--;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj.AddCar(carType);
+ */
--- Assignment7/readme.md
+++ Assignment7/readme.md
@@ -1,1 +1,1 @@
-![image caption](./assignment7_3/image.png)
+![image caption](./assignment7_4/image.png)
